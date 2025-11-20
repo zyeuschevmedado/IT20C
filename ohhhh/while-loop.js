e9084872425ea5066321082e8f0472d6ec92d9f9
zyeuschevmedado <zyeusm@gmail.com>
2025-11-20 08:40:47 +0800

    Corrected array logic on while-loop.js

---
+++ ohhhh/while-loop.js
@@ -0,0 +1,42 @@
+
+console.log("reverse")
+i = 5;
+
+while (i <= 5){
+      console.log(i);
+      i++;
+
+}
+
+console.log("ascending");
+x = 1;
+
+while (x >= 5){
+      console.log(x);
+      i++;
+
+
+}
+
+output = " ";
+x=1;
+
+while (x<-5){
+  output += x + " ";
+  x++;
+
+}
+
+console.log(output);
+
+
+arr=[1,2,3];
+output = " ";
+index = 0;
+
+while(index <= arr.length-1){
+    output += arr[index] +" ";
+    index++;
+}
+
+console.log(output);
